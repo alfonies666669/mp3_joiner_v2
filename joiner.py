d0e6a04ecc458493413965dc4714ad7ae4b22830
Serafim <santscho6666@gmail.com>
2022-05-02 15:30:57 +0300

    parse list files from directory with cmd

--- joiner.py
+++ joiner.py
@@ -1,16 +1,20 @@
 import subprocess
 import os
 
-path_to = ''
+path_to = r'C:\Users\Serafim\Downloads\books\Опасные связи (чит. Евгений Терновский)'
 
 
 def main_loop():
     cnt = 0
     common_list = []
     common_el = ''
+    cmd_files = subprocess.check_output(f'dir /b', shell=True, cwd=path_to).decode('utf-8')
+    list_files = [row.replace('\r', '') for row in cmd_files.split('\n')]
+    del list_files[-1]
     how_much = int(input("How: "))
-    files = [x.join('""') for x in os.listdir(os.chdir(path_to))]
-    max_value = int(len(files)) // int(how_much)
+    files = [x.join('""') for x in list_files]
+    print(files)
+    # max_value = int(len(files)) // int(how_much)
     while len(files) != 0:
         common_list.append(files[0:how_much])
         common_el = ['+'.join(i) for i in common_list]
